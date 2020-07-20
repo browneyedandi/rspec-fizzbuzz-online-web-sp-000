acdeee412ed9991a9847152616681da4f89fa99c
Andriana Brockman <andriana.brockman@gmail.com>
2020-07-20 21:57:28 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,8 +3,10 @@ def fizzbuzz(int)
     return "Fizz"
   elsif int % 5 == 0
     return "Buzz"
-  else int % 15 == 0
+  elsif int % 15 == 0
     return "FizzBuzz"
+  else
+    return nil
   end
 end
 
