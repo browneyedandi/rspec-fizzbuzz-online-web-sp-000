db65da9597d41a8f6f8919e9c185414886e17d01
Andriana Brockman <andriana.brockman@gmail.com>
2020-07-20 21:32:12 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 def fizzbuzz(x)
-if x == 3
-  return "Fizz"
-elsif x == 5
-  return "Buzz"
-elsif x == 15
-  return "FizzBuzz"
-end
+  if x == 3
+    return "Fizz"
+  elsif x == 5
+    return "Buzz"
+  elsif x == 15
+    return "FizzBuzz"
+  end
 end
 
